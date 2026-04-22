1c8b4f64821434432d7bdfcc132562d23afa1e9b
Dasari Nandini <nandinidasari728@gmail.com>
2026-04-22 16:10:21 +0530

    adds backend API and connect frontend to fetch/create posts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import {  format } from "date-fns";
-import {produce} from "immer"
+import { format } from "date-fns";
+import { produce } from "immer";
 
 type post = {
-  id:number,
-title:string | null,
-body:string | null,
-time:string
-}
+  id: number;
+  title: string | null;
+  body: string | null;
+  time: string;
+};
 
 type Feed = {
-  nextId:number,
-  posts:post[]
-}
+  nextId: number;
+  posts: post[];
+};
 
-type postProps =  
-  {
-    setPost: React.Dispatch<React.SetStateAction<Feed>>
-    data:Feed}
+type postProps = {
+  setPost: React.Dispatch<React.SetStateAction<Feed>>;
+  data: Feed;
+};
 const Title = () => (
   <div className="title-container">
     <p>Title</p>
-    <input type="text" name="title" placeholder="Enter a title" id="title"></input>
+    <input type="text" name="title" placeholder="Enter a title" id="title">
+    </input>
   </div>
 );
 
@@ -34,45 +35,59 @@ const Body = () => (
   </div>
 );
 
+const SinglePost = (
+  { data, ondelete }: { data: post; ondelete: () => void },
+) => (
+  <>
+    <h3>Alex Johnson</h3>
+    <p>{data.time}</p>
+    <h2>{data.title}</h2>
+    <p>{data.body}</p>
+    <button type="button" onClick={ondelete}>Delete</button>
+  </>
+);
 
-const SinglePost = ({data,ondelete}:{data:post,ondelete:() => void}) => <>
-<h3>Alex Johnson</h3>
-<p>{data.time}</p>
-<h2>{data.title}</h2>
-<p>{data.body}</p>
-<button type="button" onClick={ondelete}>Delete</button>
-</>
-
-const Feed = ({ data, setPost }: { data: Feed; setPost: React.Dispatch<React.SetStateAction<Feed>> }) => {
+const Feed = (
+  { data, setPost }: {
+    data: Feed;
+    setPost: React.Dispatch<React.SetStateAction<Feed>>;
+  },
+) => {
   return data.posts.map((post) => (
     <SinglePost
       key={post.id}
       data={post}
-      ondelete={() =>
+      ondelete={() => {
         setPost((prev) =>
           produce(prev, (draft: Feed) => {
             draft.posts = draft.posts.filter((p) => p.id !== post.id);
           })
-        )
-      }
+        );
+      }}
     />
   ));
 };
 
-const Post = ({setPost,data}:postProps) => {
-
-  const submitPost = (e:React.SubmitEvent<HTMLFormElement>) => {
+const Post = ({ setPost, data }: postProps) => {
+  const submitPost = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.target);  
+    const formData = new FormData(e.target);
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
     const time = format(new Date(Date.now()), "yyyy-MM-dd");
-    const newPost = {id:data.nextId + 1,title,body,time};
-    setPost((prev:Feed) =>
-      produce(prev, (draft) => {
-        draft.posts.push(newPost);
-        draft.nextId += 1;
-      }))
+    const newPost = { id: data.nextId + 1, title, body, time };
+
+    const addData = async () => {
+      const response = await fetch("http://localhost:8080/add", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+      });
+      const result = await response.json();
+      console.log("in add", response, result.data);
+      setPost(result.data);
+    };
+
+    addData();
   };
 
   return (
@@ -83,13 +98,27 @@ const Post = ({setPost,data}:postProps) => {
         <Body />
         <button type="submit" id="submit-btn">Post</button>
       </form>
-      <Feed data={data} setPost={setPost}/>
+      <Feed data={data} setPost={setPost} />
     </>
   );
 };
 
 const App = () => {
-  const [postList,setPost] =  useState<Feed>({nextId : 1,posts : [{id:1,title:"title of post",body:"this is my post",time:"wed 1234"}]})
-  return <Post setPost={setPost} data={postList}/>;
+  const [postList, setPost] = useState<Feed>({
+    nextId: 1,
+    posts: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/getdata");
+      const result = await response.json();
+      console.log(response, result.data);
+      setPost(result.data);
+    };
+
+    fetchData();
+  }, []);
+  return <Post setPost={setPost} data={postList} />;
 };
 export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/feed":{
-        target:"http://localhost:5173"
-      }
-    }
-  }
-})
+  server: {
+    proxy: {
+      "/feed": "/public/data/post.json",
+    },
+  },
+});
