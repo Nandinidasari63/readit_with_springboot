2e06fbf8fc231442bec0d87e2c365d522bd38889
Dasari Nandini <nandinidasari728@gmail.com>
2026-04-22 11:56:36 +0530

    Adds post form and show posts
    Handles form input and save posts in state
    Displays list of posts on screen
    Adds basic button stylingAdd post form and show posts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
+type post = {
+title:string | null,
+body:string | null
+}
+
+type postProps =  
+  {setPost:(x:post[]) => void,data:post[]}
+
+
 const Title = () => (
-  <div className="title">
+  <div className="title-container">
     <p>Title</p>
-    <input type="text" placeholder="Enter a title"></input>
+    <input type="text" name="title" placeholder="Enter a title" id="title"></input>
   </div>
 );
 
@@ -15,10 +25,32 @@ const Body = () => (
   </div>
 );
 
-const Post = () => {
+
+const SingleFeed = ({data}:{data:post}) => <>
+<h3>Alex Johnson</h3>
+<p>1234</p>
+<h2>{data.title}</h2>
+<p>{data.body}</p>
+<button type="button">Delete</button>
+</>
+
+const Feed = ({data}:{data:post[]}) => {
+return data.map((post) => 
+  <SingleFeed data={post}/>
+)
+}
+
+const Post = ({setPost,data}:postProps) => {
+  
   const submitPost = (e:React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.target);  
+    const title = formData.get("title") as string;
+    const body = formData.get("body") as string;
+    const newPost = {title,body};
+    setPost([...data,newPost])
   };
+
   return (
     <>
       <h1>Create Post</h1>
@@ -27,11 +59,13 @@ const Post = () => {
         <Body />
         <button type="submit" id="submit-btn">Post</button>
       </form>
+      <Feed data={data}/>
     </>
   );
 };
 
 const App = () => {
-  return <Post />;
+  const [post,setPost] =  useState<post[]>([{title:"title of post",body:"this is my post"}])
+  return <Post setPost={setPost} data={post}/>;
 };
 export default App;
