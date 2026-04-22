adf82731ab991b7dd160cebb26ea4beb1f48eeae
Dasari Nandini <nandinidasari728@gmail.com>
2026-04-22 13:28:14 +0530

    restructures feed state and fix post add/delete logic with Immer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import {  format } from "date-fns";
+import {produce} from "immer"
 
 type post = {
+  id:number,
 title:string | null,
-body:string | null
+body:string | null,
+time:string
+}
+
+type Feed = {
+  nextId:number,
+  posts:post[]
 }
 
 type postProps =  
-  {setPost:(x:post[]) => void,data:post[]}
-
-
+  {
+    setPost: React.Dispatch<React.SetStateAction<Feed>>
+    data:Feed}
 const Title = () => (
   <div className="title-container">
     <p>Title</p>
@@ -26,29 +35,44 @@ const Body = () => (
 );
 
 
-const SingleFeed = ({data}:{data:post}) => <>
+const SinglePost = ({data,ondelete}:{data:post,ondelete:() => void}) => <>
 <h3>Alex Johnson</h3>
-<p>1234</p>
+<p>{data.time}</p>
 <h2>{data.title}</h2>
 <p>{data.body}</p>
-<button type="button">Delete</button>
+<button type="button" onClick={ondelete}>Delete</button>
 </>
 
-const Feed = ({data}:{data:post[]}) => {
-return data.map((post) => 
-  <SingleFeed data={post}/>
-)
-}
+const Feed = ({ data, setPost }: { data: Feed; setPost: React.Dispatch<React.SetStateAction<Feed>> }) => {
+  return data.posts.map((post) => (
+    <SinglePost
+      key={post.id}
+      data={post}
+      ondelete={() =>
+        setPost((prev) =>
+          produce(prev, (draft: Feed) => {
+            draft.posts = draft.posts.filter((p) => p.id !== post.id);
+          })
+        )
+      }
+    />
+  ));
+};
 
 const Post = ({setPost,data}:postProps) => {
-  
+
   const submitPost = (e:React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.target);  
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
-    const newPost = {title,body};
-    setPost([...data,newPost])
+    const time = format(new Date(Date.now()), "yyyy-MM-dd");
+    const newPost = {id:data.nextId + 1,title,body,time};
+    setPost((prev:Feed) =>
+      produce(prev, (draft) => {
+        draft.posts.push(newPost);
+        draft.nextId += 1;
+      }))
   };
 
   return (
@@ -59,13 +83,13 @@ const Post = ({setPost,data}:postProps) => {
         <Body />
         <button type="submit" id="submit-btn">Post</button>
       </form>
-      <Feed data={data}/>
+      <Feed data={data} setPost={setPost}/>
     </>
   );
 };
 
 const App = () => {
-  const [post,setPost] =  useState<post[]>([{title:"title of post",body:"this is my post"}])
-  return <Post setPost={setPost} data={post}/>;
+  const [postList,setPost] =  useState<Feed>({nextId : 1,posts : [{id:1,title:"title of post",body:"this is my post",time:"wed 1234"}]})
+  return <Post setPost={setPost} data={postList}/>;
 };
 export default App;
